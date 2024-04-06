1c1c4ff1eea8bd33f824fda7a4a8f58db2f0a747
Deris Hardiansyah Ismail <derishardiansyah27@gmail.com>
2024-04-06 10:56:27 +0700

    add file

---
+++ Helper/responHelper.js
@@ -0,0 +1,25 @@
+const responseHelper = (res, status, data, message) => {
+  const response = {
+    status: status === 500 ? "Error" : "Success",
+    statusCode: status,
+    message: status === 500 ? "Internal Server Error" : message,
+  };
+
+  if (data) {
+    response.data = data;
+  }
+
+  if (status === 500 || status === 404 || status === 401 || status === 400) {
+    const errorMessage = status === 404 ? "API Not Found" : response.message;
+
+    return res.status(status).json({
+      status: "Error",
+      statusCode: status,
+      message: errorMessage,
+    });
+  }
+
+  return res.status(status).json(response);
+};
+
+export default responseHelper;
---
+++ server.js
@@ -0,0 +1,14 @@
+import express from "express";
+
+const port = process.env.DB_PORT || 6000;
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+export default app;
