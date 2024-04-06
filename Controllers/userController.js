7829cb50b321405d7e4c920d8c1cef92262fdd38
Deris Hardiansyah Ismail <derishardiansyah27@gmail.com>
2024-04-06 17:12:16 +0700

    profile done

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -28,7 +28,6 @@ const userController = {
         );
       }
 
-      // Check if email already exists
       conn.query(
         "SELECT * FROM user WHERE email = ?",
         [email],
@@ -45,7 +44,6 @@ const userController = {
             return responseHelper(res, 400, "", "Email sudah terdaftar");
           }
 
-          // Insert user into database with plain text password
           conn.query(
             "INSERT INTO user (email, first_name, last_name, password) VALUES (?, ?, ?, ?)",
             [email, first_name, last_name, password],
@@ -97,7 +95,6 @@ const userController = {
         );
       }
 
-      // Retrieve user from database
       conn.query(
         "SELECT * FROM user WHERE email = ?",
         [email],
@@ -135,6 +132,42 @@ const userController = {
       );
     } catch (err) {
       responseHelper(res, 500, err, "Terjadi kesalahan pada server");
+    }
+  },
+  profile: async (req, res) => {
+    try {
+      const token = req.headers.authorization.split(" ")[1];
+      const decode = jwt.verify(token, process.env.secretLogin);
+      const email = decode.email;
+
+      conn.query(
+        "SELECT email, first_name, last_name FROM user WHERE email = ?",
+        [email],
+        (error, results) => {
+          if (error) {
+            return responseHelper(
+              res,
+              500,
+              null,
+              "Terjadi kesalahan pada server"
+            );
+          }
+
+          if (results.length === 0) {
+            return responseHelper(
+              res,
+              404,
+              null,
+              "Profil pengguna tidak ditemukan"
+            );
+          }
+
+          const userProfile = results[0];
+          return responseHelper(res, 200, userProfile, "Sukses");
+        }
+      );
+    } catch (err) {
+      return responseHelper(res, 500, err, "Terjadi kesalahan pada server");
     }
   },
 };
--- Routes/userRoute.js
+++ Routes/userRoute.js
@@ -1,9 +1,11 @@
 import express from "express";
 import userController from "../Controllers/userController.js";
+import verifyToken from "../Controllers/verifyToken.js";
 
 const userRouter = express.Router();
 
 userRouter.post("/registration", userController.addUser);
 userRouter.post("/login", userController.loginUser);
+userRouter.get("/profile", verifyToken, userController.profile);
 
 export default userRouter;
