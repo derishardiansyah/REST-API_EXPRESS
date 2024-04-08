d211841da1dcb084b20b62670cd4146e00fbcc40
Deris Hardiansyah Ismail <derishardiansyah27@gmail.com>
2024-04-08 20:17:28 +0700

    change file vercel.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 createTable();
-app.use("/home", docRouter);
+app.use("/", docRouter);
 app.use("/api", userRouter);
 
 app.listen(port, () => {
